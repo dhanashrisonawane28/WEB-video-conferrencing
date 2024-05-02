7470e84cd299a133463c9933794c4602242bd4bf
dhanashrisonawane28@gmail.com <dhanashrisonawane28@gmail.com>
2024-05-02 22:42:08 +0530

    first commit

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Row, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "../styles/login.css"
+function HeroBanner() {
+  return (
+    <section id="home">
+      <Container>
+        <Row style={{ display: "flex" }}>
+          <div className="home-text">
+            {/* <div className="section-text__subtitle">Explore the whiskers</div> */}
+            <div className="section-text__title-big">
+            Unify VIT Computer Department: One Virtual Platform
+            </div>
+            <div className="section-text__body">
+              Introducing our innovative video call app, seamlessly connecting users through high-quality video and audio communication. Enjoy group calls, screen sharing, and secure conversations, redefining virtual interactions for both personal and professional use.
+            </div>
+            <a href="#download">
+              <Link to='/video-app' style={{ color: "whitesmoke" }}>
+
+                <button className="call-btn">Create A Metting
+                </button>
+              </Link>
+            </a>
+          </div>
+
+          <div className="section-image">
+            <img className="img-new" src="https://www.infince.com/wp-content/uploads/2023/05/Best_video_meeting_platform-min.png" alt="App Preview" />
+          </div>
+        </Row>
+      </Container>
+    </section>
+  );
+}
+
+export default HeroBanner;
